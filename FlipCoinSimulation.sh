3a6c925ab4285af5266f1c3a70096198e97f04d4
suresh16s <sureshthite48@gmail.com>
2020-07-12 18:45:03 +0530

    NoHeadTail

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
-flip=$(( (RANDOM % 2)+0 ))
-echo $flip
+headCount=0
+tailCount=0
+read -p "Enter the number of times you want to flip the coin:" n
+echo $n
+for (( i=0; i<$n; i++ ))
+do
+	flipCoin=$(($RANDOM%2+1))
+	if [ $flipCoin -eq 1 ]
+	then
+		echo "head wins"
+		headCount=$(($headCount+1))
+	else
+		echo "tailwins"
+		tailCount=$(($tailCount+1))
+	fi
+done
+echo "head wins:" $headCount "times"
+echo "tail wins:" $tailCount "times"
 
-if [ $flip -eq 1 ]
-then
-	echo "head:"
-elif [ $flip -eq 0 ]
-then
-	echo "tail"
-fi
