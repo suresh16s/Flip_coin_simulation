cadaaec6c81a800f60b0ad051726a5634c0be143
suresh16s <sureshthite48@gmail.com>
2020-07-12 18:42:22 +0530

    Headtail

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,2 +1,11 @@
-#!/bin/bash -x
-echo "Welcome to Flip coin simulation program"
+#!/bin/bash
+flip=$(( (RANDOM % 2)+0 ))
+echo $flip
+
+if [ $flip -eq 1 ]
+then
+	echo "head:"
+elif [ $flip -eq 0 ]
+then
+	echo "tail"
+fi
