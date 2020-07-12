becb6d4c13f04b8c2c701a92321af8c1b4f9fd58
suresh16s <sureshthite48@gmail.com>
2020-07-12 18:51:23 +0530

    IfTie

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  
+#!/bin/bash -x 
 Head=0
 Tail=0
 while [ $Head -ne 21 ] && [ $Tail -ne 21 ]
@@ -11,12 +11,32 @@ Flip=$(( RANDOM%2 ))
                 Tail=$(($Tail+1))
         fi
 done
-if [ $Head -gt $Tail ]
+if [ $Head -eq $Tail ]
 then
-	echo "Head Wons by $(($Head-$Tail)):"
-elif [ $Head -lt $Tail ]
+	while [ true ]
+	do
+		r1=$((RANDOM % 2))
+		if [ $r1 -eq 1 ]
+		then
+			Head=$(($Head + 1))
+		else
+			Tail=$(($Tail + 1))
+		fi
+
+		if [ $(($Head - $Tail)) -eq 2 ]
+		then
+			echo "Head win by 2 and count is $head"
+		else
+			if [ $(($Tail - $Head)) -eq 2 ]
+			then
+				echo "Tail win by 2 and count is $tail"
+			fi
+		fi
+	done
+elif [ $Head -gt $Tail ]
 then
-	echo "Tail Wons by $(($Tail-$Head)):"
+        echo "Head Wons by $(($Head-$Tail)):"
 else
-	echo "TIE:"
+        echo "Tail Wons by $(($Tail-$Head)):"
+
 fi
